ad768d7b6351fd2a39c37aa99dd7ee9087c26e8d
suhanthalluri04 <113932550+suhanthalluri04@users.noreply.github.com>
2023-09-29 16:16:30 -0700

    test

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,17 +13,17 @@ def get_catalog():
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         first_row = result.first()
-        print(f"gold {first_row.gold}")
+        print(f"gold bruh {first_row.gold}")
         print(f"red ml {first_row.red_ml}")
 
     # Can return a max of 20 items.
 
-        return [
-                {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": 1,
-                    "price": 50,
-                    "potion_type": [100, 0, 0, 0],
-                }
-            ]
+        # return [
+        #         {
+        #             "sku": "RED_POTION_0",
+        #             "name": "red potion",
+        #             "quantity": 1,
+        #             "price": 50,
+        #             "potion_type": [100, 0, 0, 0],
+        #         }
+        #     ]
