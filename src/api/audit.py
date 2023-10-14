8234d20970e7b23235a0be31b198b2a5949752a9
suhanthalluri04 <113932550+suhanthalluri04@users.noreply.github.com>
2023-10-14 15:01:42 -0700

    fixed audit

--- src/api/audit.py
+++ src/api/audit.py
@@ -16,13 +16,15 @@ router = APIRouter(
 def get_inventory():
     """ """
     with db.engine.begin() as connection:
-      result = connection.execute(sqlalchemy.text("SELECT gold, num_red_potions, num_red_ml, num_blue_ml, num_blue_potions, num_green_ml, num_green_potions FROM global_inventory"))
+      result = connection.execute(sqlalchemy.text("SELECT gold, num_blue_ml, num_green_ml, num_red_ml FROM global_inventory"))
       first_row = result.first()
+      result2 = connection.execute(sqlalchemy.text("SELECT name, quantity FROM catalog"))
+      potQuantities = {k:v for (k,v) in result2}
       totalml = first_row.num_red_ml + first_row.num_green_ml + first_row.num_blue_ml #change this after other potions added
-      totalPot = first_row.num_red_potions + first_row.num_green_potions + first_row.num_blue_potions
+      totalPot = potQuantities["red potion"] + potQuantities["green potion"] + potQuantities["blue potion"]
       log("Audit: Current mL", f"Red: {first_row.num_red_ml} Green: {first_row.num_green_ml} Blue: {first_row.num_blue_ml}")
       log("Audit: Current Gold", f"Gold: {first_row.gold}")
-      log("Audit: Current Potions", f"Red: {first_row.num_red_potions} Green: {first_row.num_green_potions} Blue: {first_row.num_blue_potions}")
+      log("Audit: Current Potions", ("Red:", potQuantities["red potion"], "Green:", potQuantities["green potion"], "Blue:", potQuantities["blue potion"]))
     return {"number_of_potions": totalPot, "ml_in_barrels": totalml, "gold": first_row.gold}
 
 class Result(BaseModel):
