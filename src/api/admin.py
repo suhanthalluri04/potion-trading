f9d462fcac3eba48b29382cef0e9ec0de73207a8
suhanthalluri04 <113932550+suhanthalluri04@users.noreply.github.com>
2023-10-19 22:49:39 -0700

    reset function

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
+from src.discord import log
 
 router = APIRouter(
     prefix="/admin",
@@ -14,6 +17,25 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(
+          """
+          UPDATE global_inventory SET
+          gold = 100,
+          num_red_ml = 0,
+          num_blue_ml = 0,
+          num_green_ml = 0,
+          num_dark_ml = 0
+          """
+        ))
+        connection.execute(sqlalchemy.text(
+          """
+          UPDATE catalog SET
+          quantity = 0
+          """
+        ))
+    log("Reset", "Burned to the Ground!")
+
     return "OK"
 
 
@@ -23,7 +45,7 @@ def get_shop_info():
 
     # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "Alluri Concoctions",
+        "shop_owner": "Suhanth Alluri",
     }
 
